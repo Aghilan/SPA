5c865d1073e284886d0946bfae192e77bf5a318f
aghilan <aghilan@github.com>
2017-04-08 21:58:47 +0530

    Implemented Infinite Scrolling and Adjusted Img scaling

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
-    renderCardSet()
+  var bigObject
+
+  renderCardSet()
+
+  var win = $(window);
+  win.scroll(function() {
+		var scrollHeight = $(document).height() - win.height()
+    if ( scrollHeight > win.scrollTop() - 10 && scrollHeight < win.scrollTop() + 10) {
+			renderCardSet();
+    }
+	});
 });
 
 function renderCardSet() {
@@ -8,13 +18,15 @@ function renderCardSet() {
   $(".grid-set").append(temp)
   $(".grid-set").append(temp)
 }
+
 function renderContentRow() {
   $(".grid-card").html(renderContentGrid())
   var temp = $(".grid-row").html()
   $(".grid-row").append(temp)
   $(".grid-row").append(temp)
 }
+
 function renderContentGrid() {
-  var temp =  document.getElementById("temp").innerHTML
+  var temp =  $('#temp').html()
   return temp
 }
